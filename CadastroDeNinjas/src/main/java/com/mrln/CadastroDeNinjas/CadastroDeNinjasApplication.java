752dbf64c2c4833cc5bf50025efc681ffd70cf47
MarlonDEV1Lima <ritalomarlon@gmail.com>
2025-05-31 00:50:14 -0300

    first commit

---
+++ CadastroDeNinjas/src/main/java/com/mrln/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,24 @@
+package com.mrln.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+	@RequestMapping("/")
+	String home(){
+
+	return "Olá Mundo!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(
+				CadastroDeNinjasApplication.class, args);
+	}
+
+}
